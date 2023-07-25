7b7519db70079b309f45a87d17dc0bccb91a97c7
Dmitry Seredkin (laptop) <dimsf97@gmail.com>
2023-07-26 00:59:25 +0400

    Updates

---
+++ assets/index-46e949ac.js
@@ -0,0 +1,1 @@
+import{H as t,r as e}from"./entry-client-a451672d.js";export{t as default,e as routeData};
--- assets/index-9659dde3.js
+++
@@ -1,1 +0,0 @@
-import{H as t,r as e}from"./entry-client-2761189c.js";export{t as default,e as routeData};
